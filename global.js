db5bffb29406654901fe799f51d358dbd2cd6b73
Alyvia Vaughan <alvaughan@ucsd.edu>
2026-04-17 16:04:08 -0700

    commit -m "Add global.js to all HTML files and remove navigation bars"

--- global.js
+++ global.js
@@ -4,13 +4,52 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a")
+// let navLinks = $$("nav a")
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-); 
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+// ); 
 
-if (currentLink) {
-    currentLink.classList.add('current');
-} 
+// if (currentLink) {
+//     currentLink.classList.add('current');
+// } 
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/portfolio/"
+
+let pages = [
+  { url: "",          title: "Home"     },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/",  title: "Contact"  },
+  { url: "resume/",   title: "Resume"   },
+  { url: "https://github.com/Alyvia-Vaughan", title: "GitHub" },
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    if (!url.startsWith("http")) {
+        url = BASE_PATH + url;
+    }
+
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
+
+    a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+    );
+
+    if (a.host !== location.host) {
+    a.target = "_blank";
+    }
+
+    nav.append(a);
+
+}
