6a8a25a627a85f11a784eb785924e8098c5d4058
Alyvia Vaughan <alvaughan@ucsd.edu>
2026-05-01 12:45:09 -0700

    commit =m "finished step 3 in lab 5"

--- projects/projects.js
+++ projects/projects.js
@@ -18,14 +18,9 @@ let arc = arcGenerator({ startAngle: 0, endAngle: 2 * Math.PI });
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+let data = rolledData.map(([year, count]) => ({ value: count, label: year }));
+
 let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map(d => arcGenerator(d));
@@ -45,3 +40,4 @@ data.forEach((d, idx) => {
     .attr('class', 'legend-item')
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
+
