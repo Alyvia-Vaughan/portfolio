0d528ebfb5558008b73b7e62662abf987f76d944
Alyvia Vaughan <alvaughan@ucsd.edu>
2026-05-01 12:36:24 -0700

    commit -m "finished lab 5 step 2"

--- projects/projects.js
+++ projects/projects.js
@@ -18,8 +18,15 @@ let arc = arcGenerator({ startAngle: 0, endAngle: 2 * Math.PI });
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map(d => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
@@ -31,3 +38,10 @@ arcs.forEach((arc, idx) => {
     .attr('fill', colors(idx));
 });
 
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend.append('li')
+    .attr('style', `--color:${colors(idx)}`)
+    .attr('class', 'legend-item')
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+});
